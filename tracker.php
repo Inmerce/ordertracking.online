bab3a3076f2fb80c3041c7e6ccbe30fcc5e9173c
Sam <sammanders@outlook.com>
2020-09-23 17:02:11 +0200

    Added favicon

--- tracker.php
+++ tracker.php
@@ -6,6 +6,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" href="assets/img/search-location-solid.svg">
   <title>Order Tracking Online</title>
   <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
